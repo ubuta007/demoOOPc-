6d44dc6f010e02f6870ed080345258f686efdad4
ubuta007 <nivaldo007@gmail.com>
2018-08-28 23:26:38 +0100

    foi adiciona classes m e suc h e c

---
+++ DemoOOP/DemoOOP/Mamiferos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP
+{
+    abstract class Mamiferos
+    {
+        private int patas;
+
+        public int Patas
+        {
+            get {return patas; }
+            set { patas = value; }
+        }
+
+        public Mamiferos()
+        {
+            patas = 0;
+        }
+
+        public abstract void Andar();
+
+        public abstract void Imposto(int valor);
+    }
+}
--- DemoOOP/DemoOOP/Program.cs
+++ DemoOOP/DemoOOP/Program.cs
@@ -10,6 +10,19 @@ namespace DemoOOP
     {
         static void Main(string[] args)
         {
+            var pessoa = new humano(10);
+            pessoa.Patas = 4;
+
+            var dog = new Cao("Pit Bull");
+            dog.Patas = 2;
+
+            //System.Console.WriteLine("a pessoa tem {0} paase {1} de idade", pessoa.Patas, pessoa.idade);
+            pessoa.Andar();
+            pessoa.Imposto(10);
+            //System.Console.WriteLine("a pessoa tem {0} paase {1} de idade", pessoa.Patas, pessoa.idade);
+            dog.Andar();
+            dog.Imposto(10);
+            System.Console.ReadLine();
         }
     }
 }
